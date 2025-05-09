75230cd39f1de4183a5a90bdee2ae8dd528a1ac2
Aftab3008 <shaikaftab005@mail.com>
2025-05-09 13:41:55 +0530

    Interview assignment

---
+++ Frontend_Q2/interview/index.js
@@ -0,0 +1,11 @@
+document.getElementById("inputform").addEventListener("submit",function(e){
+            const password=document.getElementById("password").value;
+            const confirmpassword=document.getElementById("confirmpassword").value;
+    
+            if(password!==confirmpassword){
+                alert("Password and confrim Password should be same");
+                return;
+            }
+    
+            alert("Successfully submitted");
+})
